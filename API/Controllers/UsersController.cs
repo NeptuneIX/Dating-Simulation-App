670861cfae26be5d811efb3dc6332a03b02526a1
NeptuneIX <=>
2025-02-14 00:18:26 +0100

    Extended functionality of API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,19 +10,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class UsersController(DataContext context) : BaseApiController
+    [Authorize]
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
-        [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
-            var users = await context.Users.ToListAsync();
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() {
+            var users = await userRepository.GetMembersAsync();
+
+            // var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
 
             return Ok(users); 
         }
 
-        [HttpGet("{id:int}")] // api/users/{id}
-        public async Task<ActionResult<AppUser>> GetUsers(int id) {
-            var user = await context.Users.FindAsync(id);
+        [HttpGet("{username}")] // api/users/{id}
+        public async Task<ActionResult<MemberDto>> GetUsers(string username) {
+            var user = await userRepository.GetMemberAsync(username);
 
             if(user == null) return NotFound();
              
