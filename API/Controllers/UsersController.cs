dad19aef667717dadf655470e3f32d662867d111
NeptuneIX <=>
2025-02-16 19:04:05 +0100

    Updating resources

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -30,6 +31,24 @@ namespace API.Controllers
              
             return user;
 
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto) {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if(username == null) return BadRequest("No username found in token");
+
+            var user = await userRepository.GetUserByUsernameAsync(username);
+
+            if(user == null) return BadRequest("Could not find user");
+
+            mapper.Map(memberUpdateDto, user);
+
+
+            if(await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update the user");
         }
     }
 }
