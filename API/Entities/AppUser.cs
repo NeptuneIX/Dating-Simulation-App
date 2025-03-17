31397d92fb871a97c939d8ef49b7cb0e70a3af97
NeptuneIX <=>
2025-03-17 19:42:01 +0100

    Likes feature implemented

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -31,6 +31,10 @@ public class AppUser {
 
     public List<Photo> Photos { get; set; } = [];
 
+    public List<UserLike> LikedByUsers { get; set; } = [];
+
+    public List<UserLike> LikedUsers { get; set; } = [];
+
     public int GetAge() {
         return DateOfBirth.CalculateAge();
     }
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,8 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Member } from '../../_models/member';
 import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEnvelope, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { LikesService } from '../../_services/likes.service';
 
 @Component({
   selector: 'app-member-card',
@@ -12,10 +13,24 @@ import { faEnvelope, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './member-card.component.css'
 })
 export class MemberCardComponent {
+  private likeService = inject(LikesService);
   faUser = faUser;
   faHeart = faHeart; 
   faEnvelope = faEnvelope;
   member = input.required<Member>();
+  hasLiked = computed(() => this.likeService.likedIds().includes(this.member().id))
 
+  toggleLike() {
+    this.likeService.toggleLike(this.member().id).subscribe({
+      next: () => {
+        if(this.hasLiked()) {
+          this.likeService.likedIds.update(ids => ids.filter(x => x != this.member().id))
+        } else {
+          this.likeService.likedIds.update(ids => [...ids, this.member().id])
+        }
+
+      }
+    });
+  }
 
 }
